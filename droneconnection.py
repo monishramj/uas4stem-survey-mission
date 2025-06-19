1a867d8552ee6a18358ad547c9e401a082ba3b37
Monish RJ <160821533+monishramj@users.noreply.github.com>
2025-06-19 15:02:05 -0500

    Updated with dronekit

--- droneconnection.py
+++ droneconnection.py
@@ -1,12 +1,27 @@
-from pymavlink import mavutil
+from dronekit import connect, mavutil
 
-testmsg = 'Hello world!' # max 50 char
-connection = mavutil.mavlink_connection('', baud=0)
-connection.wait_heartbeat()
+vehicle = connect('/dev/ttyAMA0', wait_ready=True, baud=57600)
+
+msg = vehicle.message_factory.command_long_encode(
+  1, 1,
+  mavutil.mavlink.STATUSTEXT,  # msg
+  0, # confirmation 
+  mavutil.mavlink.MAV_SEVERITY_INFO, 
+  'hello world', 0, 0, # ????
+  0, 0,
+  0)
+
+vehicle.send_mavlink(msg)
+
+# from pymavlink import mavutil
+
+# testmsg = 'Hello world!' # max 50 char
+# connection = mavutil.mavlink_connection('', baud=0)
+# connection.wait_heartbeat()
 
 
-print("Heartbeat received from system (system %u component %u)" % (connection.target_system, connection.target_component))
+# print("Heartbeat received from system (system %u component %u)" % (connection.target_system, connection.target_component))
 
 
-connection.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_INFO, testmsg.encode("utf-8"))
-print(f"Message sent: {testmsg}")
+# connection.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_INFO, testmsg.encode("utf-8"))
+# print(f"Message sent: {testmsg}")
