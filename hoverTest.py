fdaaf83fa04859a12fec5232fbbbacc6dad5ed25
Monish RJ <160821533+monishramj@users.noreply.github.com>
2025-07-02 13:20:36 -0500

    updated print lines

--- hoverTest.py
+++ hoverTest.py
@@ -13,7 +13,8 @@ while not vehicle.is_armable:
     print("Waiting to initialise...")
     time.sleep(1)
 
-print(f"Time: {time.time() - startTime}")
+connectAndInitTime = time.time() - startTime
+print(f"Initialised. Time to connect and initialise: {connectAndInitTime}")
 
 vehicle.mode = VehicleMode("GUIDED")
 vehicle.armed = True
@@ -21,19 +22,22 @@ while not vehicle.mode.name == 'GUIDED' and not vehicle.armed:
     print("Arming")
     time.sleep(1)
 
-print(f"Armed. Time: {time.time() - startTime}")
+armTime = time.time() - connectAndInitTime
+print(f"Armed. Current Time: {time.time() - startTime}, time to arm: {armTime}")
 print("Takeoff")
 
 vehicle.simple_takeoff(convertedAlt)
 
+altReachTime = time.time()
 while True:
     print(f"Altitude:{vehicle.location.global_relative_frame.alt}")
     if vehicle.location.global_relative_frame.alt>= convertedAlt * 0.95:
-        print(f"Reached target altitude. Time: {time.time() - startTime}")
+        altReachTime = time.time() - armTime
+        print(f"Reached target altitude. Current time: {time.time() - startTime}, time to reach altitude: {altReachTime}")
         break
     time.sleep(1)
 
-print("Start hovering for 5 seconds")
+print("Start hovering for 5 seconds.")
 
 vehicle.mode = VehicleMode("BRAKE")
 vehicle.armed = False
@@ -41,9 +45,11 @@ while not vehicle.mode.name=='BRAKE' and vehicle.armed:
     print("BRAKING")
     time.sleep(1)
 
-print(f"Hovering. Time: {time.time() - startTime}")
+breakTime = time.time() - altReachTime
+print(f"Hovering. Current time: {time.time() - startTime}, time to break: {breakTime}")
 time.sleep(5)
-print(f"Hovering done. Time: {time.time() - startTime}. Landing.")
+hoverTime = time.time() - breakTime
+print(f"Hovering done. Current time: {time.time() - startTime}. Landing, time to hover: {hoverTime}")
 
 vehicle.mode = VehicleMode("LAND")
 vehicle.armed = False
@@ -51,4 +57,5 @@ while vehicle.armed:
     print("LANDING")
     time.sleep(1)
 
-print(f"DONE. Total time: {time.time() - startTime} LETS GOOO")
+landingTime = time.time() - hoverTime
+print(f"DONE. Total time: {time.time() - startTime} LETS GOOO.\nTime to connect and initialise: {connectAndInitTime}\nTime to arm: {armTime}\nTime to takeoff and reach altitude: {altReachTime}\nTime to break/get into hover: {breakTime}\nTime to hover compared to 5 second coded time: {hoverTime}\nTime to land: {landingTime}")
